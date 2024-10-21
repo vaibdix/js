b1e4069c928aa37eff87313df5609b37b081719d
vaibdix <dixitvaib@gmail.com>
2024-10-21 14:16:59 +0530

    storage

---
+++ DOM/storage/script.js
@@ -0,0 +1,27 @@
+let username = 'defuser'
+let password = '12345'
+sessionStorage.setItem('myUsername', username);
+sessionStorage.setItem('myPassword', password);
+console.log(sessionStorage)
+
+let localuser = "localdata"
+let localpass = "localpass"
+localStorage.setItem('localusername', localuser);
+localStorage.setItem('localpass', localpass);
+console.log(localStorage)
+
+
+// remove -- only removes single item
+// sessionStorage.removeItem("username")
+// localStorage.removeItem("localpass")
+
+
+// clears entire thing
+// localStorage.clear()
+// sessionStorage.clear()
+
+let newvarlocal = localStorage.getItem('localuser')
+console.log(newvar)
+
+let newvarsession = sessionStorage.getItem('username')
+console.log(newvarsession)
